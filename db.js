90770e8d77b86ad2af71ed570af3fa1e27b2d16f
lo111sa <losaberidze777@yahoo.com>
2023-11-16 18:22:06 +0400

    asd

--- db.js
+++ db.js
@@ -2,9 +2,10 @@ import mariadb from "mariadb";
 
 export const db = mariadb.createPool({
   //charset: "utf8mb4",
-  host: "localhost",
+  host: "192.168.100.10",
+  port: 3306,
   user: "root",
-  password: "123temoTEMO",
+  password: "123asdASD",
   database: "eyeline",
   connectTimeout: 60000,
 });
